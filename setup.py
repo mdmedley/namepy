55a1547d1d1ab506dd02b40740093fc0c4396f9b
Marcus Medley <mdmeds@gmail.com>
2019-03-16 17:56:38 -0700

    Add setup.py

---
+++ setup.py
@@ -0,0 +1,25 @@
+from setuptools import find_packages
+from setuptools import setup
+
+with open('./namepy/version.py', 'r') as infile:
+    __version__ = infile.readline().split('=')[1].strip().strip("'")
+
+
+__description__ = 'Command-line tool to generate names for the uncreative'
+
+
+setup(
+    name='namepy',
+    author='Marcus Medley',
+    author_email='mdmeds@gmail.com',
+    version=__version__,
+    packages=find_packages(),
+    setup_requires=['pytest-runner'],
+    tests_require=['pytest'],
+    description=__description__,
+    entry_points={
+        'console_scripts': [
+            'namepy = namepy.__main__:main'
+        ]
+    }
+)
